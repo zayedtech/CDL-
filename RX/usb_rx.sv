7681459b5d6e815212fa077e7c427039d7891e03
AlmothanaAlghamdi <alghamd3@purdue.edu>
2026-04-17 08:20:27 -0400

    Update usb_rx.sv

--- RX/usb_rx.sv
+++ RX/usb_rx.sv
@@ -10,12 +10,12 @@ logic [3:0] unused2;
 logic [15:0] unused3, parallel_out;
 logic [23:0] unused4, par_out;
 
-sync syncdp(.clk(clk), .n_rst(n_rst), .async_in(dp_in), .sync_out(dp));
+sync #(.RST_VAL(1)) syncdp(.clk(clk), .n_rst(n_rst), .async_in(dp_in), .sync_out(dp));
 sync syncdm(.clk(clk), .n_rst(n_rst), .async_in(dm_in), .sync_out(dm));
 
 flex_sr #(.SIZE(2), .MSB_FIRST(0)) dpsr(.clk(clk), .n_rst(n_rst), .shift_enable(dpshift_strobe), .load_enable(1'b0), .serial_in(dp), .parallel_in(unused), .serial_out(unusedbit), .parallel_out(twobits));
 
-timer timerdp(.clk(clk), .n_rst(n_rst), .enable_timer(1'b1), .data_size(3), .bit_period(cycles),  .shift_strobe(dpshift_strobe), .packet_done(end_packet));
+timer timerdp(.clk(clk), .n_rst(n_rst), .enable_timer(1'b1), .data_size(5'b00011), .bit_period(cycles),  .shift_strobe(dpshift_strobe), .packet_done(end_packet));
 
 always_comb begin : eightEightNine
     if(end_packet) begin
@@ -58,18 +58,18 @@ end
 control_fsm CFSM(.clk(clk), .n_rst(n_rst), .new_pack(new_pack), .pid_error(pid_error), .data_1(data_1), .data_0(data_0), .out_token(out_token), .in_token(in_token), .ack(ack), .strobes_16(strobes_16), .cycles_8(cycles_8), .dm(dm), .dp(dp), .data_done(data_done),
 .clear_err(clear_err), .en_timer(en_timer), .rx_data_ready(rx_data_ready), .transfer_active(transfer_active), .flush_and_start(flush_and_start), .eop_err(eop_err), .pack_done(pack_done), .timer_16(timer_16), .timer_8(timer_8), .rx_packet(rx_packet));
 
-timer timerfsm16(.clk(clk), .n_rst(n_rst), .enable_timer(timer_16), .data_size(16), .bit_period(14'b00000000001000),  .shift_strobe(unused1), .packet_done(strobes_16));
+timer timerfsm16(.clk(clk), .n_rst(n_rst), .enable_timer(timer_16), .data_size(5'b10000), .bit_period(14'b00000000001000),  .shift_strobe(unused1), .packet_done(strobes_16));
 
 flex_counter #(.SIZE(4)) counter8 (
         .clk(clk),
         .n_rst(n_rst),
         .clear(~timer_8),  
         .count_enable(timer_8),
-        .rollover_val(8),
+        .rollover_val(4'b1000),
         .count_out(unused2),
         .rollover_flag(cycles_8));
 
-timer timerpidsync(.clk(clk), .n_rst(n_rst), .enable_timer(en_timer), .data_size(16), .bit_period(14'b00000000001000),  .shift_strobe(pidsyncshift_strobe), .packet_done(syncpid_end));
+timer timerpidsync(.clk(clk), .n_rst(n_rst), .enable_timer(en_timer), .data_size(5'b10000), .bit_period(14'b00000000001000),  .shift_strobe(pidsyncshift_strobe), .packet_done(syncpid_end));
 
 flex_sr #(.SIZE(16), .MSB_FIRST(0)) syncpidsr(.clk(clk), .n_rst(n_rst), .shift_enable(pidsyncshift_strobe), .load_enable(1'b0), .serial_in(serial_in), .parallel_in(unused3), .serial_out(unusedbit2), .parallel_out(parallel_out));
 
@@ -163,7 +163,7 @@ always_comb begin : errorChecker
     end
 end
 
-timer timerdata(.clk(clk), .n_rst(n_rst), .enable_timer(flush_and_start), .data_size(8), .bit_period(14'b00000000001000),  .shift_strobe(data_strobe), .packet_done(data_end));
+timer timerdata(.clk(clk), .n_rst(n_rst), .enable_timer(flush_and_start), .data_size(5'b01000), .bit_period(14'b00000000001000),  .shift_strobe(data_strobe), .packet_done(data_end));
 
 flex_sr #(.SIZE(24), .MSB_FIRST(0)) datasr(.clk(clk), .n_rst(n_rst), .shift_enable(data_strobe), .load_enable(1'b0), .serial_in(serial_in), .parallel_in(unused4), .serial_out(unusedbit3), .parallel_out(par_out));
 
@@ -216,5 +216,4 @@ assign rx_packet_data = par_out[7:0];
 assign rx_transfer_active = transfer_active;
 
 endmodule
-
 
