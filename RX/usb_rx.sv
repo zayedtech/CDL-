92c833fac1dae6e5d20932adeab8e1b18448605b
AlmothanaAlghamdi <alghamd3@purdue.edu>
2026-04-18 08:02:25 -0400

    Update usb_rx.sv

--- RX/usb_rx.sv
+++ RX/usb_rx.sv
@@ -2,18 +2,21 @@
 
 module usb_rx (input logic clk, n_rst, dp_in, dm_in, input logic [6:0] buffer_occupancy, output logic rx_data_ready, rx_transfer_active, rx_error, flush, store_rx_packet_data, output logic [2:0] rx_packet, output logic [7:0] rx_packet_data);
 
-logic dp, dm, dpshift_strobe, unusedbit, end_packet, serial_in, new_pack, pid_error, data_1, data_0, out_token, in_token, ack, strobes_16, cycles_8, data_done, data_done_ffin, pidsyncshift_strobe, unusedbit2, unusedbit3, maybe_ffin, maybe_ffout;
-logic clear_err, en_timer, transfer_active, eop_err, pack_done, timer_16, timer_8, unused1, syncpid_end, serial_ff, data_error, flush_and_start, data_strobe, data_end, sync_error;
-logic [1:0] unused, twobits;
+logic dp, dm, dpshift_strobe, unusedbit, unusedbit4, end_packet, serial_in, new_pack, pid_error, data_1, data_0, out_token, in_token, ack, strobes_16, cycles_8, data_done, data_done_ffin, pidsyncshift_strobe, unusedbit2, unusedbit3, maybe_ffin, maybe_ffout;
+logic clear_err, en_timer, transfer_active, eop_err, pack_done, timer_16, timer_8, unused1, syncpid_end, serial_ff, data_error, flush_and_start, data_strobe, data_end, sync_error, syncpid_end_ffout;
+logic [1:0] unused, twobits, unused5, twobitsm;
 logic [13:0] cycles; 
 logic [3:0] unused2;
 logic [15:0] unused3, parallel_out;
 logic [23:0] unused4, par_out;
 
 sync #(.RST_VAL(1)) syncdp(.clk(clk), .n_rst(n_rst), .async_in(dp_in), .sync_out(dp));
+
 sync syncdm(.clk(clk), .n_rst(n_rst), .async_in(dm_in), .sync_out(dm));
 
 flex_sr #(.SIZE(2), .MSB_FIRST(0)) dpsr(.clk(clk), .n_rst(n_rst), .shift_enable(dpshift_strobe), .load_enable(1'b0), .serial_in(dp), .parallel_in(unused), .serial_out(unusedbit), .parallel_out(twobits));
+flex_sr #(.SIZE(2), .MSB_FIRST(0)) dmsr(.clk(clk), .n_rst(n_rst), .shift_enable(dpshift_strobe), .load_enable(1'b0), .serial_in(dm), .parallel_in(unused5), .serial_out(unusedbit4), .parallel_out(twobitsm));
+
 
 timer timerdp(.clk(clk), .n_rst(n_rst), .enable_timer(1'b1), .data_size(5'b00011), .bit_period(cycles),  .shift_strobe(dpshift_strobe), .packet_done(end_packet));
 
@@ -27,14 +30,15 @@ always_comb begin : eightEightNine
 end
 
 always_comb begin : checkChange
-    if(twobits[0] == twobits[1]) begin
-        serial_ff = 1;
+    if((twobits[0] == twobits[1])) begin
+        serial_in = 1;
     end
     else begin
-        serial_ff = 0;
+        serial_in = 0;
     end
 end
 
+/*
 always_ff @(posedge clk, negedge n_rst) begin : serialInFF
     if(n_rst == 0) begin
         serial_in <= 1;
@@ -43,6 +47,7 @@ always_ff @(posedge clk, negedge n_rst) begin : serialInFF
         serial_in <= serial_ff;
     end
 end
+*/
 
 always_comb begin : startBitDetector
 
@@ -52,10 +57,9 @@ always_comb begin : startBitDetector
     else begin
         new_pack = 0;
     end
-
 end
 
-control_fsm CFSM(.clk(clk), .n_rst(n_rst), .new_pack(new_pack), .pid_error(pid_error), .data_1(data_1), .data_0(data_0), .out_token(out_token), .in_token(in_token), .ack(ack), .strobes_16(strobes_16), .cycles_8(cycles_8), .dm(dm), .dp(dp), .data_done(data_done),
+control_fsm CFSM(.clk(clk), .n_rst(n_rst), .new_pack(new_pack), .pid_error(pid_error), .data_1(data_1), .data_0(data_0), .out_token(out_token), .in_token(in_token), .ack(ack), .strobes_16(strobes_16), .cycles_8(cycles_8), .dm(twobitsm[0]), .dp(twobits[0]), .data_done(data_done),
 .clear_err(clear_err), .en_timer(en_timer), .rx_data_ready(rx_data_ready), .transfer_active(transfer_active), .flush_and_start(flush_and_start), .eop_err(eop_err), .pack_done(pack_done), .timer_16(timer_16), .timer_8(timer_8), .rx_packet(rx_packet));
 
 timer timerfsm16(.clk(clk), .n_rst(n_rst), .enable_timer(timer_16), .data_size(5'b10000), .bit_period(14'b00000000001000),  .shift_strobe(unused1), .packet_done(strobes_16));
@@ -71,12 +75,21 @@ flex_counter #(.SIZE(4)) counter8 (
 
 timer timerpidsync(.clk(clk), .n_rst(n_rst), .enable_timer(en_timer), .data_size(5'b10000), .bit_period(14'b00000000001000),  .shift_strobe(pidsyncshift_strobe), .packet_done(syncpid_end));
 
-flex_sr #(.SIZE(16), .MSB_FIRST(0)) syncpidsr(.clk(clk), .n_rst(n_rst), .shift_enable(pidsyncshift_strobe), .load_enable(1'b0), .serial_in(serial_in), .parallel_in(unused3), .serial_out(unusedbit2), .parallel_out(parallel_out));
+flex_sr #(.SIZE(16), .MSB_FIRST(0)) syncpidsr(.clk(clk), .n_rst(n_rst), .shift_enable(dpshift_strobe), .load_enable(1'b0), .serial_in(serial_in), .parallel_in(unused3), .serial_out(unusedbit2), .parallel_out(parallel_out));
+
+always_ff @(posedge clk, negedge n_rst) begin : syncPidEnd
+    if(n_rst == 0) begin
+        syncpid_end_ffout <= 0;
+    end
+    else begin
+        syncpid_end_ffout <= syncpid_end;
+    end
+end
 
 always_comb begin : decodeSyncPid
 
-    if(syncpid_end) begin
-        if(parallel_out[7:0] != 8'b00000001) begin
+    if(syncpid_end & !syncpid_end_ffout) begin
+        if(parallel_out[7:0] != 8'b10000000) begin
             sync_error = 1;
             in_token = 0;
             out_token = 0;
