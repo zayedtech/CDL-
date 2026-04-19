4ddf74ac234304ca4a42598784f8cca9de284244
Yuren Chen <142936511+Ababaovo@users.noreply.github.com>
2026-04-19 18:57:47 -0400

    Update tb_ahb_usb.sv

--- tb_ahb_usb.sv
+++ tb_ahb_usb.sv
@@ -394,22 +394,38 @@ module tb_ahb_usb();
 
     initial begin
 
-        test_name = "Status Register Read";
         n_rst = 1;
         dp_in = 1;
         dm_in = 0;
+
+        test_name = "Reset defaults";
         reset_dut();
-        sync_byte();
-        ack_pid();
-        eop();
-        #(83.33333);
-        #(83.33333);
-        enqueue_read(4'h4, 2'd1, 32'h0000_0008);
+        enqueue_read(4'h4, 2'd1, 32'h0000_0000);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h6, 2'd1, 32'h0000_0000);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h8, 2'd0, 32'h0000_0000);
         execute_transactions(1);
         finish_transactions();
 
+        test_name = "ACK status after RX done";
+        reset_dut();
+        sync_byte();
+        in_pid();
+        eop();
+        #(83.33333);
+        #(83.33333);
+        #(83.33333);
+        #(83.33333);
+        #(83.33333);
+        #(83.33333);
+        enqueue_read(4'h4, 2'd1, 32'h0000_0002);
+        execute_transactions(1);
+        finish_transactions();
 
-        test_name = "DATA0";
+        test_name = "DATA0 occupancy and status";
         reset_dut();
         sync_byte();
         data0_pid();
@@ -424,6 +440,41 @@ module tb_ahb_usb();
         enqueue_read(4'h4, 2'd1, 32'h0000_0011);
         execute_transactions(1);
         finish_transactions();
+
+        test_name = "READ DATA0 1st Byte";
+        enqueue_read(4'h0, 2'd0, 32'h0000_0055);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h8, 2'd0, 32'h0000_0001);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h4, 2'd0, 32'h0000_0011);
+        execute_transactions(1);
+        finish_transactions();
+
+        test_name = "READ DATA0 2nd Byte";
+        enqueue_read(4'h0, 2'd0, 32'h0000_00aa);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h8, 2'd0, 32'h0000_0000);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h4, 2'd0, 32'h0000_0010);
+        execute_transactions(1);
+        finish_transactions();
+
+        test_name = "DATA0 halfword read";
+        reset_dut();
+        sync_byte();
+        data0_pid();
+        random_data();
+        data_crc();
+        eop();
+        #(83.33333);
+        #(83.33333);
+        enqueue_read(4'h8, 2'd0, 32'h0000_0002);
+        execute_transactions(1);
+        finish_transactions();
         enqueue_read(4'h0, 2'd1, 32'h0000_aa55);
         execute_transactions(1);
         finish_transactions();
@@ -431,8 +482,42 @@ module tb_ahb_usb();
         execute_transactions(1);
         finish_transactions();
 
+        test_name = "OUT then DATA0 host to endpoint";
+        reset_dut();
+        sync_byte();
+        out_pid();
+        token_data_crc();
+        eop();
+        #(400ns);
 
-
+        sync_byte();
+        data0_pid();
+        random_data();
+        data_crc();
+        eop();
+        #(83.33333);
+        #(83.33333);
+        enqueue_read(4'h4, 2'd1, 32'h0000_0015);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h8, 2'd0, 32'h0000_0002);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h4, 2'd1, 32'h0000_0015);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h0, 2'd0, 32'h0000_0055);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h0, 2'd0, 32'h0000_00aa);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h8, 2'd0, 32'h0000_0000);
+        execute_transactions(1);
+        finish_transactions();
+        enqueue_read(4'h4, 2'd1, 32'h0000_0014);
+        execute_transactions(1);
+        finish_transactions();
 
         $finish;
     end
